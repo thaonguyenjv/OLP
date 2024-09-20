2b981099ef7c11fd4fbc023d93e5f4e3df69c8a7
Thao Nguyen <153426831+thaonguyenjv@users.noreply.github.com>
2024-09-20 08:33:00 +0700

    Add files via upload

---
+++ DistinctNumbers/DistinctNumbers/DistinctNumbers.cpp
@@ -0,0 +1,24 @@
+﻿// DistinctNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const long long MaxN = 2 * 1e5 + 1;
+int main()
+{
+    int n;
+	cin >> n;
+	int a[MaxN];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a+ n);
+	int dem = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] != a[i + 1]) {
+			++dem;
+		}
+	}
+	cout << dem;
+}
